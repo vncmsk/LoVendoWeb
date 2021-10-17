5d0bcfbcbaeb50df56b77f13e5a9f117b42f14f3
Andres R <yosmarandres@hotmail.com>
2021-10-17 03:30:21 -0500

    modificacion reporte de ventas

--- src/pages/reporteVentas/ReporteVentas.jsx
+++ src/pages/reporteVentas/ReporteVentas.jsx
@@ -1,57 +1,45 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useRef } from 'react'
 import Header from '../../components/encabezado/encabezado';
-// import { ToastContainer, toast } from 'react-toastify';
-// import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const ventasBack = [
     {
-        No: 1,
         Fecha: "1/1/2021",
         Cliente: "Andres Rincon",
-        Cod: "A0001",
         Item: "Gorra RedBull, Navy",
         Cantidad: 100,
         VrUnit: 7000,
-        VrTotal: 70000000,
         Vendedor: "Lina Cuartas",
         Ciudad: "Bogota",
         Estado: "Enviado"
     },
     {
-        No: 2,
         Fecha: "1/1/2021",
         Cliente: "Andres Rincon",
-        Cod: "A0001",
         Item: "Gorra RedBull, Navy",
         Cantidad: 100,
         VrUnit: 7000,
-        VrTotal: 70000000,
         Vendedor: "Lina Cuartas",
         Ciudad: "Bogota",
         Estado: "Enviado"
     },
     {
-        No: 3,
         Fecha: "1/1/2021",
         Cliente: "Andres Rincon",
-        Cod: "A0001",
         Item: "Gorra RedBull, Navy",
         Cantidad: 100,
         VrUnit: 7000,
-        VrTotal: 70000000,
         Vendedor: "Lina Cuartas",
         Ciudad: "Bogota",
         Estado: "Enviado"
     },
     {
-        No: 4,
         Fecha: "1/1/2021",
         Cliente: "Andres Rincon",
-        Cod: "A0001",
         Item: "Gorra RedBull, Red",
         Cantidad: 100,
         VrUnit: 7000,
-        VrTotal: 70000000,
         Vendedor: "Lina Cuartas",
         Ciudad: "San andres",
         Estado: "Enviado"
@@ -73,55 +61,68 @@ const ReporteVentas = () => {
                     <h1>Gestión de ventas</h1>
                 </div>
             </header>
-            <RegistroVentas />
+            <RegistroVentas agregarVenta={setVentas} listaVentas={ventas} />
             <ConsultaVentas />
             <TablaVentas listaVentas={ventas} />
+            <ToastContainer position="bottom-center" autoClose={2000} />
         </div>
     );
-}
+};
 
-const RegistroVentas = () => {
+const RegistroVentas = ({ agregarVenta, listaVentas }) => {
 
-    const [nombre, SetNombre] = useState("")
+    const form = useRef(null);
 
-    const EnviarAlBack = () => {
-        console.log('variable:', nombre);
-        <span>saquelo</span>
+    const submitFormulario = (e) => {
+        e.preventDefault();
+        const fd = new FormData(form.current);
+
+        const EnviarAlBack = {};
+        fd.forEach((value, key) => {
+            EnviarAlBack[key] = value;
+        });
+        
+        agregarVenta ([...listaVentas, EnviarAlBack])
+        toast.success('Venta registrada exitosamente')
+        toast.error('Error, venta no registrada')
     };
 
     return (
         <div className="container p-2 border border-secondary border-4 my-3">
             <label className="d-flex text-light fs-6 bg-dark">Registro de ventas</label>
-            <form className='row row-cols-5'>
-                <input type="date" className="mx-4 mt-1" name='fecha' onChange={(e) => { console.log("nombre: ", e.target.value) }} placeholder="fecha de venta" />
-                <select className="Vendedor mx-4 mt-1" name='vendedor' onChange={(e) => { console.log("nombre: ", e.target.value) }} id="inputGroupSelect01">
+            <form ref={form} className='row row-cols-5' onSubmit={submitFormulario}>
+                <input required type="date" className="mx-4 mt-1" name='Fecha' placeholder="fecha de venta" />
+                <select required className="Vendedor mx-4 mt-1" name='Vendedor' id="inputGroupSelect01">
                     <option defaultValue>Seleccione un vendedor...</option>
                     <option value="Andres">Andres</option>
                     <option value="Lina">Lina</option>
                     <option value="Maria">Maria</option>
                 </select>
-                <input className="Articulo mx-4 mt-1" onChange={(e) => { SetNombre(e.target.value) }} name='articulo' type="text" placeholder="Articulo" />
-                <select className="seleccionarCiudad mx-4 mt-1" id="inputGroupSelect01">
-                    <option name='ciudad' defaultValue>Seleccione una ciudad...</option>
-                    <option value="1">San Andres</option>
-                    <option value="2">Medellin</option>
-                    <option value="3">Bogotá</option>
+                <input required className="Articulo mx-4 mt-1" name='Item' type="text" placeholder="Articulo" />
+                <input required className="Articulo mx-4 mt-1" name='Cliente' type="text" placeholder="Cliente" />
+                <select required className="seleccionarCiudad mx-4 mt-1" name='Ciudad' id="inputGroupSelect01">
+                    <option defaultValue>Seleccione una ciudad...</option>
+                    <option value="Bogota">Bogotá</option>
+                    <option value="San Andres">San Andres</option>
+                    <option value="Medellin">Medellin</option>
                 </select>
-                <select className="seleccionarEstado mx-4 my-1" name='estado' id="inputGroupSelect01">
+                <select required className="seleccionarEstado mx-4 my-1" name='Estado' id="inputGroupSelect01">
                     <option defaultValue>Seleccione estado...</option>
-                    <option value="1">Solicitado</option>
-                    <option value="2">Enviado</option>
-                    <option value="3">Entregado</option>
+                    <option value="Solicitado">Solicitado</option>
+                    <option value="Enviado">Enviado</option>
+                    <option value="Entregado">Entregado</option>
                 </select>
-                <input className="mx-4 my-1" type="number" name='cantidad' placeholder="Cantidad" />
-                <input className="mx-4 my-1" type="number" name='valor' placeholder="valor" />
-                <button type="button" onClick={EnviarAlBack} className='bg-warning mx-4 my-1'>Registrar venta</button>
+                <input required className="mx-4 my-1" type="number" name='Cantidad' placeholder="Cantidad" />
+                <input required className="mx-4 my-1" type="number" name='VrUnit' placeholder="valor" />
+                <button type="submit" className='bg-warning mx-4 mt-4'>Registrar venta</button>
             </form>
         </div>
     );
 };
 
 const ConsultaVentas = () => {
+
+
     return (
         <div className="container p-2 border border-secondary border-4">
             <label className="d-flex text-light fs-6 bg-dark">Consulta de ventas</label>
@@ -180,14 +181,11 @@ const TablaVentas = ({ listaVentas }) => {
 
                 <thead>
                     <tr className="text-center">
-                        <th scope="col ">#</th>
                         <th scope="col ">Fecha</th>
                         <th scope="col ">Cliente</th>
-                        <th scope="col ">Cod</th>
                         <th scope="col ">Item</th>
                         <th scope="col ">Cantidad</th>
                         <th scope="col ">Vr. Unit</th>
-                        <th scope="col ">Vr. Total</th>
                         <th scope="col ">Vendedor</th>
                         <th scope="col ">Ciudad</th>
                         <th scope="col ">Estado Pedido</th>
@@ -198,19 +196,17 @@ const TablaVentas = ({ listaVentas }) => {
                     {listaVentas.map((ventas) => {
                         return (
                             <tr key={ventas.No}>
-                            <td>{ventas.No}</td>
-                            <td>{ventas.Fecha}</td>
-                            <td>{ventas.Cliente}</td>
-                            <td>{ventas.Cod}</td>
-                            <td>{ventas.Item}</td>
-                            <td>{ventas.Cantidad}</td>
-                            <td>${ventas.VrUnit}</td>
-                            <td>${ventas.VrTotal}</td>
-                            <td>{ventas.Vendedor}</td>
-                            <td>{ventas.Ciudad}</td>
-                            <td>{ventas.Estado}</td>
-                        </tr>
-                    )})}
+                                <td>{ventas.Fecha}</td>
+                                <td>{ventas.Cliente}</td>
+                                <td>{ventas.Item}</td>
+                                <td>{ventas.Cantidad}</td>
+                                <td>${ventas.VrUnit}</td>
+                                <td>{ventas.Vendedor}</td>
+                                <td>{ventas.Ciudad}</td>
+                                <td>{ventas.Estado}</td>
+                            </tr>
+                        )
+                    })}
                 </tbody>
             </table>
         </div>
